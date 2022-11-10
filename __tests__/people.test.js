6b9c82805fe5b1c3d19d6a42e4e1aabefe728983
kevinnail <fumalicious@gmail.com>
2022-11-10 12:08:11 -0800

    env file updated, people table created, getAll test/ model/ controller created, route set

---
+++ __tests__/people.test.js
@@ -0,0 +1,74 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+// const { request } = require('../lib/app.js');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('people controller', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('GET /people should return a list of all the people', async () => {
+    const resp = await request(app).get('/people');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "first_name": "Chris",
+          "id": "1",
+          "last_name": "Issitt",
+        },
+        Object {
+          "first_name": "Tracy",
+          "id": "2",
+          "last_name": "Blees",
+        },
+        Object {
+          "first_name": "Upton",
+          "id": "3",
+          "last_name": "Angless",
+        },
+        Object {
+          "first_name": "Jocelyn",
+          "id": "4",
+          "last_name": "Romaines",
+        },
+        Object {
+          "first_name": "Constantine",
+          "id": "5",
+          "last_name": "Witherop",
+        },
+        Object {
+          "first_name": "Diandra",
+          "id": "6",
+          "last_name": "Schimek",
+        },
+        Object {
+          "first_name": "Seumas",
+          "id": "7",
+          "last_name": "Hallifax",
+        },
+        Object {
+          "first_name": "Reena",
+          "id": "8",
+          "last_name": "Crowthe",
+        },
+        Object {
+          "first_name": "Wash",
+          "id": "9",
+          "last_name": "Fandrich",
+        },
+        Object {
+          "first_name": "Olav",
+          "id": "10",
+          "last_name": "Sneezum",
+        },
+      ]
+    `);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,26 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS people;
+
+CREATE TABLE people(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL
+);
+
+INSERT INTO
+    people (first_name,last_name)
+VALUES
+('Chris','Issitt'),
+('Tracy','Blees'),
+('Upton','Angless'),
+('Jocelyn','Romaines'),
+('Constantine','Witherop'),
+('Diandra','Schimek'),
+('Seumas','Hallifax'),
+('Reena','Crowthe'),
+('Wash','Fandrich'),
+('Olav','Sneezum');
+
+
+
